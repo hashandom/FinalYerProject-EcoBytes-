a61795bb8a22da470a5696c448cfc837b15b4497
hashan <sampathdh199@gmail.com>
2026-03-01 13:45:04 +0530

    Initial project setup with docker mysql

---
+++ src/test/java/com/example/EcoBytes/EcoBytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoBytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoBytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
