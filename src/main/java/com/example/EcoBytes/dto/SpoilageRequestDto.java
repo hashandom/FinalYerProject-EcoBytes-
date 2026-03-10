70cfc5f6307b4bbcdaff9f9b4e3edbdebee3967a
hashan <sampathdh199@gmail.com>
2026-03-11 00:52:38 +0530

    did some changes

--- src/main/java/com/example/EcoBytes/dto/SpoilageRequestDto.java
+++ src/main/java/com/example/EcoBytes/dto/SpoilageRequestDto.java
@@ -2,17 +2,24 @@ package com.example.EcoBytes.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class SpoilageRequestDto {
 
-    @NotBlank
+    @NotBlank(message = "Batch ID is required")
     private String batchId;
 
-    @NotBlank
+    @NotBlank(message = "Reason is required")
     private String reason;
 
-    @Min(1)
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
     private Integer quantity;
+
+    @NotNull(message = "data is required")
+    private LocalDate date;
 }
